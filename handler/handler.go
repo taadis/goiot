1ee9202661b51b2b36e8d7e7890f0dbdb094a2b7
taadis <i@taadis.com>
2022-02-01 23:19:44 +0800

    use model message

--- handler/handler.go
+++ handler/handler.go
@@ -1,14 +1,17 @@
 package handler
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/taadis/goiot/model"
 )
 
 const topic string = "topic/test"
+const topicGoiot string = "topic/goiot"
 
 func OnConnect(client mqtt.Client) {
 	log.Printf("Connected\n")
@@ -24,20 +27,40 @@ func MessagePublishHandler(client mqtt.Client, msg mqtt.Message) {
 
 // 消息订阅回调
 func MessageSubscribeCallback(client mqtt.Client, message mqtt.Message) {
-	log.Printf("Subscribed to message id:%d and payload:%+v", message.MessageID(), message.Payload())
+	var data model.Model
+	err := json.Unmarshal(message.Payload(), &data)
+	if err != nil {
+		log.Printf("Subscribed to meesage json.Unmarshal error:%+v", err)
+		return
+	}
+
+	log.Printf("Subscribed to message id:%d and payload:%+v", message.MessageID(), data)
 }
 
 func Sub(client mqtt.Client) {
-	token := client.Subscribe(topic, 1, MessageSubscribeCallback)
+	token := client.Subscribe(topicGoiot, 1, MessageSubscribeCallback)
 	token.Wait()
 	log.Printf("Subscribed to topic %s", topic)
 }
 
 func Publish(client mqtt.Client) {
 	num := 10
+	// for i := 0; i < num; i++ {
+	// 	text := fmt.Sprintf("Message %d", i)
+	// 	token := client.Publish(topic, 1, false, text)
+	// 	token.Wait()
+	// 	time.Sleep(time.Second)
+	// }
 	for i := 0; i < num; i++ {
-		text := fmt.Sprintf("Message %d", i)
-		token := client.Publish(topic, 1, false, text)
+		data := &model.Model{}
+		data.IDnum = int64(i)
+		data.MachineIP = fmt.Sprintf("127.0.0.%d", i)
+		payload, err := json.Marshal(data)
+		if err != nil {
+			log.Printf("publish data json.Marshal error:%+v", err)
+			continue
+		}
+		token := client.Publish(topicGoiot, 1, false, payload)
 		token.Wait()
 		time.Sleep(time.Second)
 	}
